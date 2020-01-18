b03040743696998f24b408d31107ed23234ddd2a
Alfarady <radenalfa9@gmail.com>
2020-01-18 21:32:42 +0700

    first

--- database/migrations/2020_01_18_134530_create_deputies_table.php
+++ database/migrations/2020_01_18_134530_create_deputies_table.php
@@ -16,6 +16,7 @@ class CreateDeputiesTable extends Migration
         Schema::create('deputies', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('name_bagian');
             $table->timestamps();
         });
     }
