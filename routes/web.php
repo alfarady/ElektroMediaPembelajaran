7180bc589dafbc7fd17a6c973eda91ec688b32fb
Alfarady Raja <radenalfa9@gmail.com>
2020-03-15 15:14:00 +0700

    new boilerplate

--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,4 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
     Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
 
     Route::resource('users', 'UsersController');
-
-    Route::delete('products/destroy', 'ProductsController@massDestroy')->name('products.massDestroy');
-
-    Route::resource('deputies', 'DeputyController');
-
-    Route::resource('categories', 'CategoryController');
-
-    Route::resource('subcategories', 'SubcategoryController');
-
-    Route::get('letters/get_ref_no/{deputy_id}/{category_id}/{sub_category_id}', 'LetterController@getRefNo');
-    Route::get('letters/get_cat/{id}', 'LetterController@getCat');
-    Route::get('letters/get_sub_cat/{id}', 'LetterController@getSubCat');
-    Route::resource('letters', 'LetterController');
-    Route::resource('instance-settings', 'InstanceSettingsController');
 });
